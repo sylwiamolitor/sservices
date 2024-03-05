01d89a278966b99c286c94e5ffed027eaa2b60fa
Sylwia <sylwiamlr@gmail.com>
2024-03-05 21:11:29 +0100

    Added OpenAPI documentation.

--- customer/src/main/java/org/sylwia/customer/CustomerController.java
+++ customer/src/main/java/org/sylwia/customer/CustomerController.java
@@ -1,5 +1,7 @@
 package org.sylwia.customer;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
+@Tag(name = "Operations on customers")
 @AllArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
 
     @PostMapping
+    @Operation(summary = "Method for new customer registration.")
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("New customer registration: {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
--- fraud/src/main/java/org/sylwia/fraud/FraudController.java
+++ fraud/src/main/java/org/sylwia/fraud/FraudController.java
@@ -1,21 +1,26 @@
 package org.sylwia.fraud;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.sylwia.clients.fraud.FraudCheckHistoryResponse;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
 @Slf4j
+@Tag(name = "Operations on frauds")
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
 
     @GetMapping(path = "{customerId}")
+    @Operation(summary = "Method for checking if customer is fraudulent.")
     public FraudCheckHistoryResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         log.info("Fraud check request for customer: {}", customerId);
